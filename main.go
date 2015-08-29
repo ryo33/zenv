708f444d7c798fb5a32d51a252c72a67c1059062
ryo33 <ryo12redstone@gmail.com>
2015-08-29 20:46:17 +0900

    Make main.go clean

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"github.com/codegangsta/cli"
 	"github.com/ryo33/zenv/commands"
-	"github.com/ryo33/zenv/environment"
-	"github.com/ryo33/zenv/util"
 	"os"
 )
 
@@ -13,52 +11,8 @@ func main() {
 	app.Name = "zenv"
 	app.Usage = "powerful environments management"
 	app.Commands = commands.Commands
-	app.Before = setup
-	app.Flags = []cli.Flag{
-		cli.StringFlag{
-			Name:  "global, g",
-			Usage: "edit global environment",
-		},
-		cli.StringFlag{
-			Name:  "local, l",
-			Usage: "edit local environment",
-		},
-		cli.BoolFlag{
-			Name:  "quiet, q",
-			Usage: "do not print normal output",
-		},
-		cli.BoolFlag{
-			Name:  "debug, d",
-			Usage: "print debugging information",
-		},
-	}
-	app.Action = doZenv
+	app.Before = commands.ZenvSetup
+	app.Flags = commands.ZenvFlag
+	app.Action = commands.DoZenv
 	app.Run(os.Args)
-}
-
-func setup(c *cli.Context) error {
-	if c.Bool("quiet") {
-		util.Quiet = true
-	}
-	if c.Bool("debug") {
-		util.Debug = true
-	}
-	if g := c.String("global"); len(g) != 0 {
-		commands.Env = environment.GetGlobalEnv(g)
-	}
-	if l := c.String("local"); len(l) != 0 {
-		commands.Env = environment.GetLocalEnv(l)
-	}
-	return nil
-}
-
-func doZenv(c *cli.Context) {
-	args := c.Args()
-	if len(args) == 1 {
-		for _, env := range environment.GetActivated(args[0]) {
-			util.Print(env)
-		}
-	} else {
-		util.PrintErrorMessage("needs 1 arg")
-	}
 }
